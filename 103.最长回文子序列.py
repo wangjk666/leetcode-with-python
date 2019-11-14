6100b108497d11555f99b79022ec571986832048
wangjk <wjk990724@163.com>
2019-11-14 08:05:58 -0800

    11.14 commmit

---
+++ 103.最长回文子序列.py
@@ -0,0 +1,22 @@
+def longestPalindromeSubseq(s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    length = len(s)
+    f = [[0 for i in range(length)] for j in range(length)] 
+    for i in range(length-1, -1, -1):
+        f[i][i] = 1
+        for j in range(i+1, length):
+            if s[i] == s[j]:
+                f[i][j] = f[i+1][j-1] + 2
+            else:
+                f[i][j] = max(f[i+1][j], f[i][j-1])
+    return f[0][length-1]
+
+
+print(longestPalindromeSubseq("cbadc"))
+
+
+
+        
